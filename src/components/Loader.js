ce6f49c04ea0b1bafba5805b681b427c19c36854
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-04-18 12:50:06 +0500

    qasimOk

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,24 +1,24 @@
-import React from "react";
-import CircularProgress from "@mui/material/CircularProgress";
-import { makeStyles } from "@mui/styles";
+// import React from "react";
+// import CircularProgress from "@mui/material/CircularProgress";
+// import { makeStyles } from "@mui/material";
 
-const useStyles = makeStyles((theme) => ({
-  loaderContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh",
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   loaderContainer: {
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     height: "100vh",
+//   },
+// }));
 
-const Loader = () => {
-  const classes = useStyles();
+// const Loader = () => {
+//   const classes = useStyles();
 
-  return (
-    <div className={classes.loaderContainer}>
-      <CircularProgress />
-    </div>
-  );
-};
+//   return (
+//     <div className={classes.loaderContainer}>
+//       <CircularProgress />
+//     </div>
+//   );
+// };
 
-export default Loader;
+// export default Loader;
