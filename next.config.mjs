98ca5688f0576e9e61299829c6e3297ed500912d
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-04-23 12:48:04 +0500

    qasim tahir

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-    // serverComponentsExternalPackages: ["mongoose"],
-    serverActions: true,
-  },
+  // reactStrictMode: true,
+  // experimental: {
+  //   appDir: true,
+  //   serverComponentsExternalPackages: ["mongoose"],
+  //   serverActions: true,
+  // },
 };
 
 export default nextConfig;
