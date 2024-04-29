a8576568150299505c03d9be032ac7ece5d3c197
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-04-29 10:37:14 +0500

    qasim4

--- src/app/adminPanel/Adminlayout.js
+++ src/app/adminPanel/Adminlayout.js
@@ -1,27 +1,27 @@
-"use client";
-// components/AdminLayout.js
-import { React, useState } from "react";
-import AdminHeader from "./AdminHeader/index";
-import AdminSidebar from "./AdminSidebar/index";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
+// "use client";
+// // components/AdminLayout.js
+// import { React, useState } from "react";
+// import AdminHeader from "./AdminHeader/index";
+// import AdminSidebar from "./AdminSidebar/index";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import Box from "@mui/material/Box";
 
-const AdminLayout = ({ children }) => {
-  // const theme = useTheme();
-  // const [open, setOpen] = useState(false);
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [submenuOpen, setSubmenuOpen] = useState(false);
+// const AdminLayout = ({ children }) => {
+//   // const theme = useTheme();
+//   // const [open, setOpen] = useState(false);
+//   const [anchorEl, setAnchorEl] = useState(null);
+//   const [submenuOpen, setSubmenuOpen] = useState(false);
 
-  const [open, setOpen] = useState(true);
+//   const [open, setOpen] = useState(true);
 
-  return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AdminHeader open={open} setOpen={setOpen} />
-      <AdminSidebar open={open} setOpen={setOpen} />
-      {children}
-    </Box>
-  );
-};
+//   return (
+//     <Box sx={{ display: "flex" }}>
+//       <CssBaseline />
+//       <AdminHeader open={open} setOpen={setOpen} />
+//       <AdminSidebar open={open} setOpen={setOpen} />
+//       {children}
+//     </Box>
+//   );
+// };
 
-export default AdminLayout;
+// export default AdminLayout;
