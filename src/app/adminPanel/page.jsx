7604c8803bd971fdb405de4ec054df4b4bdbd2b2
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-04-29 13:11:15 +0500

    qasim6

--- src/app/adminPanel/page.jsx
+++ src/app/adminPanel/page.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+// "use client";
+// import React from "react";
 
-export default function AdminPanel(props) {
+// import Adminlayout from "@/adminPanel/Adminlayout";
+// import DashboardEcommerce from "@/../components/Dashboard/E-commerce";
+
+// export default function HeroSection(props) {
+//   return (
+//     <Adminlayout>
+//       <div style={{ marginTop: "65px", padding: "20px" }}>
+//         <h3>Dashboard Advanced here</h3>
+//         <DashboardEcommerce />
+//       </div>
+//     </Adminlayout>
+//   );
+// }
+
+"use client";
+import React, { useEffect } from "react";
+
+import Adminlayout from "@/adminPanel/Adminlayout";
+import DashboardEcommerce from "@/../components/Dashboard/E-commerce";
+
+export default function HeroSection(props) {
+  // Execute code only in the browser environment
+  useEffect(() => {
+    // Your browser-specific code here
+    // For example, if you have code that interacts with the window object
+    // or relies on browser-specific APIs, you can place it here
+  }, []);
+
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Adminlayout>
+      <div style={{ marginTop: "65px", padding: "20px" }}>
+        <h3>Dashboard Advanced here</h3>
+        {/* <DashboardEcommerce /> */}
+      </div>
+    </Adminlayout>
   );
 }
