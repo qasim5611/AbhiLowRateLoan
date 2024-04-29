5aebb04b254da86eb970d2a333122d2d7ebd8bab
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-04-29 10:35:02 +0500

    qasim3

--- src/app/adminPanel/page.js
+++ src/app/adminPanel/page.js
@@ -23,17 +23,12 @@ import DashboardEcommerce from "@/../components/Dashboard/E-commerce";
 
 export default function HeroSection(props) {
   // Execute code only in the browser environment
-  useEffect(() => {
-    // Your browser-specific code here
-    // For example, if you have code that interacts with the window object
-    // or relies on browser-specific APIs, you can place it here
-  }, []);
 
   return (
-    <Adminlayout>
-      <div style={{ marginTop: "65px", padding: "20px" }}>
-        <h3>Dashboard Advanced here</h3>
-      </div>
-    </Adminlayout>
+    // <Adminlayout>
+    <div style={{ marginTop: "65px", padding: "20px" }}>
+      <h3>Dashboard Advanced here</h3>
+    </div>
+    // </Adminlayout>
   );
 }
