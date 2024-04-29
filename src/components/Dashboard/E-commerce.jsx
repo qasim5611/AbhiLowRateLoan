e1f00ebe38096d5fe5d28f2c2c7c2d137a647866
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-04-29 13:47:26 +0500

    new

--- src/components/Dashboard/E-commerce.jsx
+++ src/components/Dashboard/E-commerce.jsx
@@ -1,31 +1,27 @@
 "use client";
 import React from "react";
-// import ChartOne from "../Charts/ChartOne";
-// import ChartThree from "../Charts/ChartThree";
-// import ChartTwo from "../Charts/ChartTwo";
-// import ChatCard from "../Chat/ChatCard";
-// import TableOne from "../Tables/TableOne";
-// import CardDataStats from "../CardDataStats";
-// import MapOne from "../Maps/MapOne";
+import ChartThree from "../Charts/ChartThree";
+import ChartTwo from "../Charts/ChartTwo";
+import ChatCard from "../Chat/ChatCard";
+import CardDataStats from "../CardDataStats";
+import MapOne from "../Maps/MapOne";
 
-// import { styled } from "@mui/material/styles";
-// import Box from "@mui/material/Box";
-// import Paper from "@mui/material/Paper";
-// import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
 
-// const Item = styled(Paper)(({ theme }) => ({
-//   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-//   ...theme.typography.body2,
-//   padding: theme.spacing(1),
-//   textAlign: "center",
-//   color: theme.palette.text.secondary,
-// }));
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 const ECommerce = () => {
   return (
     <>
-      <h1>Ecommerce Qasiom ok</h1>
-      {/* <Grid container spacing={2}>
+      <Grid container spacing={2}>
         <Grid item xs={3}>
           <CardDataStats
             title="Total views"
@@ -153,19 +149,24 @@ const ECommerce = () => {
         <Grid item xs={12}>
           <MapOne />
         </Grid>
-      </Grid> */}
+      </Grid>
+
+      {/* <div>
+        <TableOne />
+      </div> */}
+      <ChatCard />
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+        {/* <ChartOne /> */}
+        {/* <ChartTwo />
+        <ChartThree /> */}
+        {/* <MapOne /> */}
+        {/* <div className="col-span-12 xl:col-span-8">
+          <TableOne />
+        </div>
+        <ChatCard /> */}
+      </div>
     </>
   );
 };
 
 export default ECommerce;
-
-// import React from "react";
-
-// export function ECommerce(props) {
-//   return (
-//     <>
-//       <h1>Ecommerce</h1>
-//     </>
-//   );
-// }
