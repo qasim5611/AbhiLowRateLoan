22beb8986f3fc602927afb5b1be579f60800f333
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-04-29 14:05:24 +0500

    qasim

--- src/components/Dashboard/E-commerce.jsx
+++ src/components/Dashboard/E-commerce.jsx
@@ -6,17 +6,17 @@ import React from "react";
 // import CardDataStats from "../CardDataStats";
 import MapOne from "../Maps/MapOne";
 
-import { styled } from "@mui/material/styles";
+// import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: "center",
+//   color: theme.palette.text.secondary,
+// }));
 
 const ECommerce = () => {
   return (
