a3be3e6ddbfcfd69cbf9ad114fe4f7646fd81eaa
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-05-10 22:01:31 +0500

    qas

---
+++ src/app/adminPanel/featuredTop/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Adminlayout from "../layout";
+
+export default function HeroSection(props) {
+  return (
+    <Adminlayout>
+      <h3>Featured Top Section</h3>
+    </Adminlayout>
+  );
+}
---
+++ src/app/adminPanel/herosection/page.js
@@ -0,0 +1,54 @@
+"use client";
+
+import React from "react";
+
+import Adminlayout from "../layout";
+
+import Breadcrumb from "@/../components/Breadcrumbs/Breadcrumb";
+import { Grid, TextField } from "@mui/material";
+
+export default function HeroSection(props) {
+  return (
+    <Adminlayout>
+      <div style={{ marginTop: "65px", padding: "20px", width: "100%" }}>
+        <Breadcrumb pageName="Home Banner" />
+
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <label for="heroSec" className="lblform">
+              Banner Tag Line:
+            </label>
+
+            <textarea id="heroSec" name="w3review" rows="3" cols="30">
+              Please Enter:
+            </textarea>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <label for="heroSec" className="lblform">
+              Refinance Button Text
+            </label>
+            <br />
+            <input type="text" className="textfild" placeholder="(Left)" />
+          </Grid>
+
+          <Grid item xs={6}>
+            <label for="heroSec" className="lblform">
+              Lown Button Text
+            </label>
+            <br />
+            <input type="text" className="textfild" placeholder="(Right)" />
+          </Grid>
+        </Grid>
+        <br />
+        <Grid container spacing={2}>
+          <Grid item>
+            <button>Update Home Banner</button>
+          </Grid>
+        </Grid>
+      </div>
+    </Adminlayout>
+  );
+}
