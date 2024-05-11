4770169346ce4463abee932bcd1bd414a0b56cd5
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-05-11 10:53:03 +0500

    qass

--- src/app/adminPanel/page.jsx
+++ src/app/adminPanel/page.jsx
@@ -2,14 +2,15 @@
 import React, { useEffect } from "react";
 
 import Adminlayout from "@/adminPanel/layout";
-import DashboardEcommerce from "@/../components/Dashboard/E-commerce";
+// import DashboardEcommerce from "@/../components/Dashboard/E-commerce";
 
 export default function HeroSection() {
   return (
     <Adminlayout>
       <div style={{ marginTop: "65px", padding: "20px" }}>
         {/* <h3>Dashboard Advanced here</h3> */}
-        <DashboardEcommerce />
+        {/* <DashboardEcommerce /> */}
+        <h1>AdminPanel</h1>
       </div>
     </Adminlayout>
   );
