e915ae2b8e7a8f1c4132c838bbea3be1b0ed2fc7
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-06-09 14:32:50 +0500

    final1

--- src/app/adminPanel/page.jsx
+++ src/app/adminPanel/page.jsx
@@ -2,30 +2,30 @@
 
 "use client";
 import React, { useEffect, useState } from "react";
-import { useSession, signIn, signOut } from "next-auth/react";
+// import { useSession, signIn, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 import Adminlayout from "@/adminPanel/Adminlayout";
 import DashboardEcommerce from "@/../components/Dashboard/E-commerce";
 
 export default function HeroSection() {
-  const { data: session, status } = useSession();
+  // const { data: session, status } = useSession();
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    if (status === "unauthenticated") {
-      signIn(); // Redirect to login page if not authenticated
-    } else if (session && session.user.role !== "admin") {
-      router.push("/"); // Redirect to home if not admin
-    } else if (session && session.user.role === "admin") {
-      setIsLoading(false); // Allow rendering if admin
-    }
-  }, [session, status, router]);
+  // useEffect(() => {
+  //   if (status === "unauthenticated") {
+  //     signIn(); // Redirect to login page if not authenticated
+  //   } else if (session && session.user.role !== "admin") {
+  //     router.push("/"); // Redirect to home if not admin
+  //   } else if (session && session.user.role === "admin") {
+  //     setIsLoading(false); // Allow rendering if admin
+  //   }
+  // }, [session, status, router]);
 
-  if (isLoading) {
-    return <div>Loading...</div>; // Show loading while checking auth status
-  }
+  // if (isLoading) {
+  //   return <div>Loading...</div>; // Show loading while checking auth status
+  // }
 
   return (
     <Adminlayout>
