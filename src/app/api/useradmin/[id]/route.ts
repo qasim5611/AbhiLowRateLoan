aa572fc1f9ad066c407e0f582cbf61609078009a
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-07-01 09:19:23 +0500

    qas3

--- src/app/api/useradmin/[id]/route.ts
+++ src/app/api/useradmin/[id]/route.ts
@@ -1,23 +1,23 @@
-import { NextRequest, NextResponse } from "next/server"; 
-import { DeleteImage } from "../../../../lib/upload-images";
-// import { FeatureTopSection } from "../../../../modals/FeatureTopModel";
-import { ImageGallaryModal } from "../../../../modals/FeatureTopModel";
+// import { NextRequest, NextResponse } from "next/server"; 
+// import { DeleteImage } from "../../../../lib/upload-images";
+// // import { FeatureTopSection } from "../../../../modals/FeatureTopModel";
+// import { ImageGallaryModal } from "../../../../modals/FeatureTopModel";
 
 
-export const DELETE = async(req: NextRequest, ctx:{params:{ id: string }})=>{ 
- const result_delete =  await DeleteImage(ctx.params.id);
+// export const DELETE = async(req: NextRequest, ctx:{params:{ id: string }})=>{ 
+//  const result_delete =  await DeleteImage(ctx.params.id);
 
- await ImageGallaryModal.findOneAndDelete({
-  public_id: ctx.params.id,
- });
+//  await ImageGallaryModal.findOneAndDelete({
+//   public_id: ctx.params.id,
+//  });
 
 
-  return NextResponse.json(
- {msg : result_delete},
-    { 
-      status: 200  
-    }
-  )
-}
+//   return NextResponse.json(
+//  {msg : result_delete},
+//     { 
+//       status: 200  
+//     }
+//   )
+// }
 
 
