017d9ed48dce83153afba1c2b7fd0505ebe2dac4
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-07-08 09:50:21 +0500

    parallex mobile2

--- src/components/Parallex/parallex.jsx
+++ src/components/Parallex/parallex.jsx
@@ -8,14 +8,14 @@ const Parallax = () => {
   const matches = useMediaQuery("(max-width:600px)");
   const matches2 = useMediaQuery("(min-width:600px)");
   return (
-    <>
+    <Box>
       <Box sx={matches2 ? { display: "block" } : { display: "none" }}>
         <div className={styles.parallax}></div>
       </Box>
       <Box sx={matches ? { display: "block" } : { display: "none" }}>
         <Image src="/parallax-mobile.webp" width={500} height={600} />
       </Box>
-    </>
+    </Box>
   );
 };
 
