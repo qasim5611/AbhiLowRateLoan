08db57deb0b78a255a4bd1ab99afb774d92a204d
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-07-08 09:55:10 +0500

    parallex back

--- src/components/Parallex/parallex.jsx
+++ src/components/Parallex/parallex.jsx
@@ -1,22 +1,8 @@
 import React from "react";
 import styles from "./Parallax.module.css";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import Box from "@mui/material/Box";
-import Image from "next/image";
 
 const Parallax = () => {
-  const matches = useMediaQuery("(max-width:600px)");
-  const matches2 = useMediaQuery("(min-width:600px)");
-  return (
-    <Box>
-      <Box sx={matches2 ? { display: "block" } : { display: "none" }}>
-        <div className={styles.parallax}></div>
-      </Box>
-      <Box sx={matches ? { display: "block" } : { display: "none" }}>
-        <Image src="/parallax-mobile.webp" width={500} height={600} />
-      </Box>
-    </Box>
-  );
+  return <div className={styles.parallax}></div>;
 };
 
 export default Parallax;
