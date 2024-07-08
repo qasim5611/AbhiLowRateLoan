13b15b0f69e6a4f8aeaabe4ddc693d268a98d12f
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-07-08 10:37:45 +0500

    imageBlurring

--- src/app/api/useradmin/featuretopget/route.ts
+++ src/app/api/useradmin/featuretopget/route.ts
@@ -1,8 +1,11 @@
 import { NextRequest, NextResponse } from "next/server"; 
+import { Connect } from "../../../../dbconfig/dbconfig";
+
 import { UploadImage } from "./../../../../lib/upload-images";
 // import { FeatureTopSection } from "../../../../modals/FeatureTopModel";
 import { ImageGallaryModal } from "../../../../modals/FeatureTopModel";
 export const GET = async(req: NextRequest)=>{ 
+  await Connect();
   console.log("Get Route initiated");
  const Images = await ImageGallaryModal.find({});
  console.log("Images at server", Images);
