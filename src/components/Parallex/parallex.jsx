b928238ccb3d0cbc0862daa9e305d1d779913680
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-07-08 10:46:05 +0500

    imageBlurring final

--- src/components/Parallex/parallex.jsx
+++ src/components/Parallex/parallex.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import styles from "./Parallax.module.css";
+import { Box, useMediaQuery } from "@mui/material";
+import Image from "next/image";
 
 const Parallax = () => {
-  return <div className={styles.parallax}></div>;
+  const matches1 = useMediaQuery("(min-width:600px)");
+  const matches = useMediaQuery("(max-width:600px)");
+  return (
+    <Box>
+      <Box sx={matches1 ? { display: "block" } : { display: "none" }}>
+        <div className={styles.parallax}></div>
+      </Box>
+      <Box sx={matches ? { display: "block" } : { display: "none" }}>
+        <Image src="/parallax-mobile.webp" height={550} width={500} />
+      </Box>
+    </Box>
+  );
 };
 
 export default Parallax;
