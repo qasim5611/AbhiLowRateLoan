2b1eb71cc696ad9d0c2ba5c7e72ced64ab3fc77c
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-08-08 18:18:55 +0500

    reputeableLender

--- src/components/ReputableLenders.js
+++ src/components/ReputableLenders.js
@@ -89,7 +89,53 @@ export function ReputableLenders(props) {
                   })}
                 </>
               ) : (
-                <>Loading...</>
+                <div className="kt-inside-inner-col">
+                  <h2
+                    className="kt-adv-heading_4fee88-b0 wp-block-kadence-advancedheading"
+                    data-kb-block="kb-adv-heading_4fee88-b0"
+                  >
+                    <strong>Home Loans from over 53+ Reputable Lenders</strong>
+                  </h2>
+
+                  <div className="wp-block-kadence-image kb-image_12d362-b2 desktop-lender-img">
+                    <figure className="aligncenter size-large">
+                      <img
+                        decoding="async"
+                        loading="lazy"
+                        style={{
+                          margin: "0px auto",
+                        }}
+                        src="/pics/home-loans-lenders.webp"
+                        alt="home loans lenders list"
+                        className="kb-img wp-image-4131"
+                        // srcSet="https://scoresbymortgagebroker.com.au/wp-content/uploads/2023/01/home-loans-lenders-1024x432.webp 1024w, https://scoresbymortgagebroker.com.au/wp-content/uploads/2023/01/home-loans-lenders-300x127.webp 300w, https://scoresbymortgagebroker.com.au/wp-content/uploads/2023/01/home-loans-lenders-768x324.webp 768w, https://scoresbymortgagebroker.com.au/wp-content/uploads/2023/01/home-loans-lenders.webp 1366w"
+                        // sizes="(max-width: 1024px) 100vw, 1024px"
+                      />
+                    </figure>
+                  </div>
+
+                  <div className="wp-block-image mobile-lender-img">
+                    <figure className="aligncenter size-full">
+                      <img
+                        decoding="async"
+                        loading="lazy"
+                        width="594"
+                        height="829"
+                        src="/images/lenders-vertical.webp"
+                        alt="loan lenders"
+                        className="wp-image-4718"
+                        srcSet="/images/lenders-vertical.webp"
+                        sizes="(max-width: 594px) 100vw, 594px"
+                      />
+                    </figure>
+                  </div>
+
+                  <div className="wp-block-kadence-spacer aligncenter kt-block-spacer-_a8a064-c7">
+                    <div className="kt-block-spacer kt-block-spacer-halign-center">
+                      <hr className="kt-divider" />
+                    </div>
+                  </div>
+                </div>
               )}
             </div>
           </div>
