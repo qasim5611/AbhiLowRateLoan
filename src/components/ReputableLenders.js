92c70b24538f85f115461776df306723959754f1
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-09-09 00:33:15 +0500

    qas

--- src/components/ReputableLenders.js
+++ src/components/ReputableLenders.js
@@ -3,31 +3,49 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
-  Box,
   Button,
   CircularProgress,
-  Grid,
   TextField,
+  Grid,
   Typography,
 } from "@mui/material";
 import { getReputeableBanner } from "./../redux/slices/globalSlice";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+// import Grid from "@mui/material/Grid2";
+
+import { styled } from "@mui/material/styles";
+import Image from "next/image";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { display } from "@mui/system";
 export function ReputableLenders(props) {
+  const Item = styled(Paper)(({ theme }) => ({
+    // backgroundColor: "#fff",
+    // ...theme.typography.body2,
+    // padding: theme.spacing(1),
+    // textAlign: "center",
+    // color: theme.palette.text.secondary,
+    // ...theme.applyStyles("dark", {
+    //   backgroundColor: "#1A2027",
+    // }),
+  }));
   let dispatch = useDispatch();
 
   const [record2, setrecord2] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      let resp = await dispatch(getReputeableBanner());
-      console.log(".....>>>>>>>getReputeableBanner", resp?.payload?.data?.data);
-      setrecord2(resp?.payload?.data?.data);
-    };
-    fetchData();
-  }, []);
+  const matches = useMediaQuery("(max-width:600px)");
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     let resp = await dispatch(getReputeableBanner());
+  //     console.log(".....>>>>>>>getReputeableBanner", resp?.payload?.data?.data);
+  //     setrecord2(resp?.payload?.data?.data);
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <>
-      <div className="wp-block-kadence-rowlayout alignnone">
+      {/* <div className="wp-block-kadence-rowlayout alignnone">
         <div
           id="kt-layout-id_85ee71-82"
           className="kt-row-layout-inner kt-layout-id_85ee71-82"
@@ -140,7 +158,523 @@ export function ReputableLenders(props) {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
+      <Box style={{ display: matches ? "none" : "block" }}>
+        <Grid container spacing={2} className="parenlogonewbrand">
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/CBA-CAN_CMYK_Coated.jpg"
+                alt="brand"
+                width={160}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/firstmac.jpg"
+                alt="brand"
+                width={180}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Auswide-Bank-Logo-2021.png"
+                alt="brand"
+                width={200}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/ING_LOGO_COLOUR_RGB.JPG"
+                alt="brand"
+                width={200}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/heritage bank.jpg"
+                alt="brand"
+                width={200}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} className="parenlogonewbrand">
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/New ME Bank logo-01-RGB.jpg"
+                alt="brand"
+                width={100}
+                height={100}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/NEW BoM_Alt_Horizontal Stacked_POS_RGB.jpg"
+                alt="brand"
+                width={200}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/LS-Logo-_0000_LS-Colour.jpg"
+                alt="brand"
+                width={150}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/STG MASTER LOGO.PNG"
+                alt="brand"
+                width={180}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/amp.jpg"
+                alt="brand"
+                width={150}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} className="parenlogonewbrand">
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Teachers-Mutual-Bank-600.jpg"
+                alt="brand"
+                width={200}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/MyState_Logox2.png"
+                alt="brand"
+                width={180}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Bluestone Secondary Logo .jpg"
+                alt="brand"
+                width={180}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/bank_west_australia.JPG"
+                alt="brand"
+                width={150}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Health Professional bank .png"
+                alt="brand"
+                width={200}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} className="parenlogonewbrand">
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/NAB Horizontal TAB_RGB.JPG"
+                alt="brand"
+                width={120}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Suncorp_Hor_CMYK.jpg"
+                alt="brand"
+                width={140}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/CHL-Final-RGB.jpg"
+                alt="brand"
+                width={170}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Ubank.webp"
+                alt="brand"
+                width={250}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Resimac logo.jpeg"
+                alt="brand"
+                width={90}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} className="parenlogonewbrand">
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>&nbsp;</Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>&nbsp;</Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Mac logo with isolation zone (macquarie final)-01.png"
+                alt="brand"
+                width={110}
+                height={100}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>&nbsp;</Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>&nbsp;</Box>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Box style={{ marginTop: "70px", display: matches ? "block" : "none" }}>
+        <Grid container spacing={2} className="parenlogonewbrandmob">
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/CBA-CAN_CMYK_Coated.jpg"
+                alt="brand"
+                width={130}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/firstmac.jpg"
+                alt="brand"
+                width={140}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <br />
+          <br />
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Auswide-Bank-Logo-2021.png"
+                alt="brand"
+                width={150}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/ING_LOGO_COLOUR_RGB.JPG"
+                alt="brand"
+                width={150}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/heritage bank.jpg"
+                alt="brand"
+                width={150}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/New ME Bank logo-01-RGB.jpg"
+                alt="brand"
+                width={80}
+                height={100}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} className="parenlogonewbrandmob">
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/NEW BoM_Alt_Horizontal Stacked_POS_RGB.jpg"
+                alt="brand"
+                width={150}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/LS-Logo-_0000_LS-Colour.jpg"
+                alt="brand"
+                width={120}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/STG MASTER LOGO.PNG"
+                alt="brand"
+                width={150}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/amp.jpg"
+                alt="brand"
+                width={130}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Teachers-Mutual-Bank-600.jpg"
+                alt="brand"
+                width={170}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/MyState_Logox2.png"
+                alt="brand"
+                width={150}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} className="parenlogonewbrandmob">
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Bluestone Secondary Logo .jpg"
+                alt="brand"
+                width={160}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/bank_west_australia.JPG"
+                alt="brand"
+                width={130}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Health Professional bank .png"
+                alt="brand"
+                width={170}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/NAB Horizontal TAB_RGB.JPG"
+                alt="brand"
+                width={100}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Suncorp_Hor_CMYK.jpg"
+                alt="brand"
+                width={120}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/CHL-Final-RGB.jpg"
+                alt="brand"
+                width={150}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} className="parenlogonewbrandmob">
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Ubank.webp"
+                alt="brand"
+                width={250}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Resimac logo.jpeg"
+                alt="brand"
+                width={90}
+                height={200}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Box style={{ textAlign: "center" }}>
+              <Image
+                src="/pics/Mac logo with isolation zone (macquarie final)-01.png"
+                alt="brand"
+                width={90}
+                height={100}
+                style={{ margin: "0px auto" }}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
     </>
   );
 }
