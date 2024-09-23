552c666ac6a7ccf7552977dc23aa7190f767363c
qasimtahir5611 <141336580+qasimtahir5611@users.noreply.github.com>
2024-09-23 17:57:06 +0500

    sitemapall

--- build-utilities/generate-sitemap.js
+++ build-utilities/generate-sitemap.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { url } = require("inspector");
 const path = require("path");
 
 function generateSitemap() {
@@ -13,6 +14,155 @@ function generateSitemap() {
       url: "https://lowratehomeloan.com.au/about-us",
       changefreq: "weekly",
     },
+    {
+      url: "https://lowratehomeloan.com.au/buy-a-new-home",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/refinance-home-loan",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/buy-investment-property",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/compare-rates",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/areas-we-service",
+      changefreq: "weekly",
+    },
+    // Explicitly adding all mortgage broker pages as individual static URLs
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-altona",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-berwick",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-burwood",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-caroline-springs",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-clyde",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-craigieburn",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-cranbourne",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-dandenong",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-fitzroy",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-footscray",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-glen-waverley",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-hampton-park",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-hoppers-crossing",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-keysborough",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-laverton",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-mitcham",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-mornington-peninsula",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-mount-waverley",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-pakenham",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-ringwood",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-rockbank",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-scoresby",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-south-yarra",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-springvale",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-tarneit",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-wheelers-hill",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-williams-landing",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-wyndham-vale",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-sydney-nsw",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-perth",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/rose-bay-mortgage-brokers",
+      changefreq: "weekly",
+    },
+    {
+      url: "https://lowratehomeloan.com.au/mortgage-broker-brisbane",
+      changefreq: "weekly",
+    },
   ];
 
   const sitemap = generateSitemapXml(pages);
@@ -22,21 +172,20 @@ function generateSitemap() {
 
 function generateSitemapXml(pages) {
   let xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-`;
-
-  pages.forEach((page) => {
-    xml += `
-  <url>
-    <loc>${page.url}</loc>
-    <changefreq>${page.changefreq}</changefreq>
-    <priority>${page.priority || "0.5"}</priority>
-  </url>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     `;
+
+  pages.forEach((pages) => {
+    xml += `
+        <url>
+        <loc> ${pages.url} </loc>
+        <changefreq> ${pages.changefreq} </changefreq>
+        <priority> ${pages.priority || "0.5"}  </priority>
+
+        </url>
+        `;
   });
 
-  xml += `</urlset>`; // Proper closing tag
+  xml += `<urlset>`;
   return xml;
 }
-
-generateSitemap();
--- next-sitemap.js
+++ next-sitemap.js
@@ -1,13 +1,13 @@
+// // /** @type {import('next-sitemap').IConfig} */
+// // const sitemapConfig = {
+// //   siteUrl: "https://lowratehomeloan.com.au",
+// //   generateRobotsTxt: true,
+// //   sitemapSize: 5000,
+// // };
+// // module.exports = sitemapConfig;
 // /** @type {import('next-sitemap').IConfig} */
-// const sitemapConfig = {
-//   siteUrl: "https://lowratehomeloan.com.au",
-//   generateRobotsTxt: true,
-//   sitemapSize: 5000,
+// module.exports = {
+//   siteUrl: "https://lowratehomeloan.com.au", // Replace with your site URL
+//   generateRobotsTxt: true, // (Optional) Generate robots.txt
+//   sitemapSize: 5000, // Limit the number of URLs per sitemap
 // };
-// module.exports = sitemapConfig;
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://lowratehomeloan.com.au", // Replace with your site URL
-  generateRobotsTxt: true, // (Optional) Generate robots.txt
-  sitemapSize: 5000, // Limit the number of URLs per sitemap
-};
